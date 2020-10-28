e1fc185ed004b348142d8bbee5190c3b5b85286f
jeffreybanket <jeffreybanket@gmail.com>
2020-10-28 14:49:05 +0100

    nog ermee bezig

--- src/Auto.java
+++ src/Auto.java
@@ -1,2 +1,35 @@
+import java.io.PrintStream;
 public class Auto {
+    private int snelheid;
+    private String merk;
+    private final int kilometerstand;
+    public static String Testritje() {
+        return "testritje";
+    }
+    public <auto1> Auto() {
+        snelheid = 0;
+        merk = merk;
+        kilometerstand = 1000;
+    }
+
+    public Auto(String type) {
+        snelheid = 0;
+        this.merk = merk;
+        kilometerstand = 2000;
+    }
+
+    public String set;
+
+    public void setSnelheid(int snelheid) {
+        this.snelheid = snelheid;
+    }
+
+    public Object getSnelheid() {
+        PrintStream printf = System.out.printf("100KM");
+        return 100;
+    }
+
 }
+
+
+
---
+++ src/Testritje.java
@@ -0,0 +1,18 @@
+public class Testritje {
+
+
+public static void main (String[] args){
+
+    Auto auto1 = new Auto(Auto.Testritje());
+System.out.println("Toyota");
+    System.out.printf("Snelheid: %s%n", auto1.getSnelheid());
+    Auto auto2 = new Auto(Auto.Testritje());
+
+    System.out.println(auto1);
+    System.out.println(auto2);
+
+}
+
+public String getSnelheid;
+
+}
